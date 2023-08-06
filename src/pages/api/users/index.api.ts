6f506055f9b2549c848e3745afccc1f89559d39b
gustavoZuin237 <zuingustavo206@gmail.com>
2023-08-06 20:43:48 -0300

    chore: finalized finished custom NextAuth adapter; Fixed a routing bug in the Register page

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '../../../../src/lib/prisma'
-
 import { setCookie } from 'nookies'
+import { prisma } from '../../../lib/prisma'
 
 export default async function handler(
   req: NextApiRequest,
@@ -21,16 +20,19 @@ export default async function handler(
 
   if (userExists) {
     return res.status(400).json({
-      message: 'Username already taken',
+      message: 'Username already taken.',
     })
   }
 
   const user = await prisma.user.create({
-    data: { name, username },
+    data: {
+      name,
+      username,
+    },
   })
 
-  setCookie({ res }, '@ignitecall: userId', user.id, {
-    maxAge: 60 * 60 * 24 * 7, // 7 dias
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
     path: '/',
   })
 
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,29 +1,26 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
-import { Container, Form, FormError, Header } from './styles'
-
+import { AxiosError } from 'axios'
+import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
-
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { api } from '../../lib/axios'
 
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-
-import { api } from '../../../src/lib/axios'
-import { AxiosError } from 'axios'
+import { Container, Form, FormError, Header } from './styles'
 
 const registerFormSchema = z.object({
   username: z
     .string()
-    .min(3, { message: 'O usuário precisa ter pelo menos 3 caracteres' })
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
     .regex(/^([a-z\\-]+)$/i, {
-      message: 'O usuário só pode conter letras e hífens',
+      message: 'O usuário pode ter apenas letras e hifens.',
     })
     .transform((username) => username.toLowerCase()),
   name: z
     .string()
-    .min(3, { message: 'O nome precisa ter pelo menos 3 caracteres' }),
+    .min(3, { message: 'O nome precisa ter pelo menos 3 letras.' }),
 })
 
 type RegisterFormData = z.infer<typeof registerFormSchema>
@@ -52,6 +49,8 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+
+      await router.push('/register/connect-calendar')
     } catch (err) {
       if (err instanceof AxiosError && err?.response?.data?.message) {
         alert(err.response.data.message)
@@ -76,22 +75,24 @@ export default function Register() {
 
       <Form as="form" onSubmit={handleSubmit(handleRegister)}>
         <label>
-          <Text size={'sm'}>Nome de usuário</Text>
+          <Text size="sm">Nome de usuário</Text>
           <TextInput
             prefix="ignite.com/"
-            placeholder="seu-usuario"
+            placeholder="seu-usuário"
             {...register('username')}
           />
+
           {errors.username && (
-            <FormError size={'sm'}>{errors.username.message}</FormError>
+            <FormError size="sm">{errors.username.message}</FormError>
           )}
         </label>
 
         <label>
-          <Text size={'sm'}>Nome completo</Text>
+          <Text size="sm">Nome completo</Text>
           <TextInput placeholder="Seu nome" {...register('name')} />
+
           {errors.name && (
-            <FormError size={'sm'}>{errors.name.message}</FormError>
+            <FormError size="sm">{errors.name.message}</FormError>
           )}
         </label>
 
