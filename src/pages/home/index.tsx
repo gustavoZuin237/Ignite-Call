5b3ac061ab85fe055c56d1ae129e0fb972a3749a
Gustavo Zuin <zuingustavo206@gmail.com>
2023-08-16 20:23:10 -0300

    chore: SEO optimization for deployment

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { Heading, Text } from '@ignite-ui/react'
 import { Container, Hero, Preview } from './styles'
+import { NextSeo } from 'next-seo'
 
 import Image from 'next/image'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -9,26 +10,33 @@ import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size={'4xl'}>Agendamento descomplicado</Heading>
-        <Text size={'lg'}>
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
 
-        <ClaimUsernameForm />
-      </Hero>
+      <Container>
+        <Hero>
+          <Heading size={'4xl'}>Agendamento descomplicado</Heading>
+          <Text size={'lg'}>
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
 
-      <Preview>
-        <Image
-          src={previewImage}
-          height={400}
-          quality={100}
-          priority
-          alt="Calendário simbolizando aplicação em funcionamento"
-        />
-      </Preview>
-    </Container>
+          <ClaimUsernameForm />
+        </Hero>
+
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            quality={100}
+            priority
+            alt="Calendário simbolizando aplicação em funcionamento"
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
